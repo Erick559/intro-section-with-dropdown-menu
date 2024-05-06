24827d1bc5c0fe3f486057f265a6d9a72970102e
Erick559 <erickungu82@gmail.com>
2024-05-06 13:34:00 +0300

    feat: Add two functions that handle the opening and closing of the navigation menu.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('script')
+const openNavigationMenu = document.querySelector('.hamburger-menu');
+const closeNavigationMenu = document.querySelector('.close-menu');
+const mobileNavigationMenu = document.querySelector('.mobile-nav')
+const overlay = document.querySelector('.overlay');
+
+
+openNavigationMenu.addEventListener('click',openMenu);
+closeNavigationMenu.addEventListener('click',closeMenu);
+
+
+function openMenu() {
+    overlay.classList.add('active');
+    mobileNavigationMenu.classList.add('active');
+}
+
+function closeMenu() {
+    overlay.classList.remove('active');
+    mobileNavigationMenu.classList.remove('active');
+}
+
