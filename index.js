6ac8ea422e4de6247d7efc736966be9e76d33137
Erick559 <erickungu82@gmail.com>
2024-05-06 15:22:54 +0300

    feat: Add functionality for the dropdown menu.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+// Open Mobile Navigation
 const openNavigationMenu = document.querySelector('.hamburger-menu');
 const closeNavigationMenu = document.querySelector('.close-menu');
 const mobileNavigationMenu = document.querySelector('.mobile-nav')
 const overlay = document.querySelector('.overlay');
 
-
 openNavigationMenu.addEventListener('click',openMenu);
 closeNavigationMenu.addEventListener('click',closeMenu);
+
+// DropDown
+const navListItem = document.querySelectorAll('.nav-list-item')
+navListItem.forEach(item => {
+    const navListTitle = item.querySelector('.nav-list-item-title');
+    const dropDown = item.querySelector('.nav-list-item-list');
+
+    navListTitle.addEventListener('click',()=>{
+        item.classList.toggle('active');
+    })
+})
 
 
 function openMenu() {
@@ -17,4 +28,3 @@ function closeMenu() {
     overlay.classList.remove('active');
     mobileNavigationMenu.classList.remove('active');
 }
-
